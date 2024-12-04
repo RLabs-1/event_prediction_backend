541a581d56b44c501124e6af3f4f6ee3440eae81
husam.shweiki <hussamshweiki112@gmail.com>
2024-12-04 12:55:32 +0200

    Set Database configuration

--- log_prediction_backend/settings.py
+++ log_prediction_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'log_prediction_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event_prediction_db',
+        'USER': 'husamshweiki',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
