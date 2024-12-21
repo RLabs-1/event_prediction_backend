846a7332638a317741884e6b0718a02511763ec6
husam.shweiki <hussamshweiki112@gmail.com>
2024-12-21 18:35:27 +0200

    Added Documentation to user model

--- user_management/models/models.py
+++ user_management/models/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(AbstractBaseUser):
     """
-    Base User Model
+    Base User Model Implementation
     """
     email = models.EmailField(unique=True, max_length=255)
     name = models.CharField(max_length=255)
