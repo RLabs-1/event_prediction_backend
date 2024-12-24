59023f19bf99d87e2c56b0921c2cdd64bed6a2b9
Zbedat Majd <zbedat.majd2000@gmail.com>
2024-12-24 16:09:57 +0200

    The task BET-27 completed

--- user_management/views/views.py
+++ user_management/views/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 # views.py
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from user_management.services.services import UserService
+
+
+class ActivateUserView(APIView):
+    def patch(self, request, userId):
+        """
+          Activates the user account for given user id.
+        """
+        service_response = UserService.activate_user(userId)
+        if service_response['success']:
+            return Response(service_response, status=status.HTTP_200_OK)
+        return Response(service_response, status=status.HTTP_400_BAD_REQUEST)
