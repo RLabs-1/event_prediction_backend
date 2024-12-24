bd18460e9f9d1ee2c4b63854b413a13015315ccf
Majd Zbedat <100697182+Majd-Zbedat@users.noreply.github.com>
2024-12-24 23:52:05 +0200

    Update services.py
    I updated the return message in the function to improve clarity and usability of the feedback provided to the user. Previously, the message was: return {"message": f"User {user.email} activated successfully."} 
    updated to :                 return {"message": f"User {user_id} activated successfully."}

--- user_management/services/services.py
+++ user_management/services/services.py
@@ -12,7 +12,7 @@ class UserService:
             if not user.is_active:
                 user.is_active = True
                 user.save()
-                return {"message": f"User {user.email} activated successfully."}
+                return {"message": f"User {user_id} activated successfully."}
             return {"message": "User is already active."}
         except ObjectDoesNotExist:
             return {"message": "User not found!."}
