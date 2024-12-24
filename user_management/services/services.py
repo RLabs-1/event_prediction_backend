0e6ab770f84241ed6dc3e6435d8a298cc8a17a56
Zbedat Majd <zbedat.majd2000@gmail.com>
2024-12-24 22:20:28 +0200

    Save changes in services.py and views.py

---
+++ user_management/services/services.py
@@ -0,0 +1,18 @@
+from user_management.models.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+class UserService:
+    @staticmethod
+    def activate_user(user_id):
+        """
+        Activates the user account by setting the field of is_active to True.
+        """
+        try:
+            user = User.objects.get(id=user_id)
+            if not user.is_active:
+                user.is_active = True
+                user.save()
+                return {"message": f"User {user.email} activated successfully."}
+            return {"message": "User is already active."}
+        except ObjectDoesNotExist:
+            return {"message": "User not found!."}
