2e5d43cd3ae4ea3c602bb2323ca6da6ba95d27ad
Raneem34 <53892084+Raneem34@users.noreply.github.com>
2024-12-25 02:46:23 +0200

    Exposed /api/user endpoint in urls and added user view

--- user_management/views/views.py
+++ user_management/views/views.py
@@ -1,4 +1,14 @@
+#defining the view for the api user
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# views.py
+def user_view(request):
+    #A function that handles requests to /api/user
+    #I still don't know what specific data we need, so meanwhile I used these, but I can replace it later with actual database queries
+
+    user_data = {
+        'username': 'example_user',
+        'email': 'user@example.com'
+    }
+    return JsonResponse(user_data)
 
