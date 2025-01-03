6373530669d33096484e208e79afb393c7a2030f
Haya Masoud <hayamasoud099@gmail.com>
2025-01-03 14:51:17 +0200

    Implement password reset functionality

---
+++ user_management/services/services.py
@@ -0,0 +1,77 @@
+
+from user_management.models.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+
+
+class UserService:
+    @staticmethod
+    def activate_user(user_id):
+        """
+        Activates the user account by setting the field of is_active to True.
+        """
+        try:
+            user = User.objects.get(id=user_id)
+            if not user.is_active:
+                user.is_active = True
+                user.save()
+                return {"message": f"User {user_id} activated successfully."}
+            return {"message": "User is already active."}
+        except ObjectDoesNotExist:
+            return {"message": "User not found!."}
+
+
+
+
+
+
+
+class RegistrationService:
+    @staticmethod
+    def register_user(validated_data):
+
+        user = User(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            is_active=True,
+            is_staff=False
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+
+class JWTService:
+    @staticmethod
+    def create_token(user):
+        """
+        Create a JWT token for the given user using HS256 algorithm.
+        """
+        refresh = RefreshToken.for_user(user)
+        refresh.algorithm = 'HS256'
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+    @staticmethod
+    def get_user(token):
+        """
+        Get the user from the given token.
+        """
+        try:
+            # First, try to decode as an access token
+            validated_token = AccessToken(token)
+            user_id = validated_token['user_id']
+            return User.objects.get(id=user_id)
+        except (TokenError, InvalidToken, User.DoesNotExist):
+            try:
+                # If it's not an access token, try as a refresh token
+                validated_token = RefreshToken(token)
+                user_id = validated_token['user_id']
+                return User.objects.get(id=user_id)
+            except (TokenError, InvalidToken, User.DoesNotExist):
+                return None
--- user_management/views/views.py
+++ user_management/views/views.py
@@ -1,4 +1,142 @@
-from django.shortcuts import render
-
 # views.py
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from user_management.services.services import RegistrationService
+from user_management.serializers.serializers import RegistrationSerializer, UserUpdateSerializer
+from rest_framework import generics,  permissions
+from user_management.models.models import User
+from django.shortcuts import render
+from user_management.services.services import UserService
+from django.contrib.auth import authenticate
+from user_management.services.services import JWTService
+from drf_spectacular.utils import extend_schema
+
+
+User = get_user_model()
+
+
+class RegistrationView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user_data = serializer.validated_data
+            user_service = RegistrationService()
+            user = user_service.register_user(user_data)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserUpdateView(generics.RetrieveUpdateAPIView):
+    """View for updating User details"""
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+    lookup_field = 'id'
+
+    http_method_names = ['patch']
+
+    def get_object(self):
+        # Ensure the user is retrieved based on the URL parameter
+        user_id = self.kwargs.get('user_id')
+        return generics.get_object_or_404(User, id=user_id)
+class ActivateUserView(APIView):
+    def patch(self, request, userId):
+        """
+          Activates the user account for given user id.
+        """
+        service_response = UserService.activate_user(userId)
+        if service_response['success']:
+            return Response(service_response, status=status.HTTP_200_OK)
+        return Response(service_response, status=status.HTTP_400_BAD_REQUEST)
+class UserLoginView(APIView):
+    """
+    Handles user login and returns a JWT token.
+    """
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if not email or not password:
+            return Response({"error": "Email and password are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        # Authenticate user
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            tokens = JWTService.create_token(user) # Generate JWT token using JWTService
+            return Response({
+                "refresh": tokens["refresh"],
+                "access": tokens["access"],
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@extend_schema(
+    summary="Reset Forgotten Password",
+    description="This endpoint allows users to reset their forgotten password by providing the email, verification code, and new password.",
+    request={
+        'type': 'object',
+        'properties': {
+            'email': {'type': 'string', 'format': 'email'},
+            'new_password': {'type': 'string'},
+            'confirm_password': {'type': 'string'},
+            'verification_code': {'type': 'string'}
+        }
+    },
+    responses={
+        200: {'description': 'Password reset successfully'},
+        400: {'description': 'Bad Request'},
+        403: {'description': 'Forbidden'},
+        404: {'description': 'User not found'},
+        410: {'description': 'Verification code expired'},
+    }
+)
+class ResetForgotPasswordView(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            email = data.get('email')
+            new_password = data.get('new_password')
+            confirm_password = data.get('confirm_password')
+            verification_code = data.get('verification_code')
+
+            if new_password != confirm_password:
+                return Response({"error": "Passwords do not match"}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Retrieve the user based on email
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+            # Check if the verification code is -1 (Forbidden)
+            if user.verification_code == '-1':
+                return Response({"error": "Forbidden"}, status=status.HTTP_403_FORBIDDEN)
+
+            # Check if the token has expired
+            if user.is_token_expired():
+                return Response({"error": "Verification code expired"}, status=status.HTTP_410_GONE)
+
+            # Check if the verification code matches
+            if user.verification_code != verification_code:
+                return Response({"error": "Invalid verification code"}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Reset the password and update the necessary fields
+            user.set_password(new_password)
+            user.is_password_reset_pending = False
+            user.verification_code = '-1'
+            user.save()
+
+            return Response({"message": "Password reset successfully"}, status=status.HTTP_200_OK)
+
+        except json.JSONDecodeError:
+            return Response({"error": "Invalid JSON"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
