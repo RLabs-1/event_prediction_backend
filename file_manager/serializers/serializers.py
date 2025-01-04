aaebf114eea7985c49abd7925aacdb8440ffcf20
NEW PC <osmanlama.2000@gmail.com>
2025-01-04 18:50:17 +0200

    Add serializers for EventSystem

--- file_manager/serializers/serializers.py
+++ file_manager/serializers/serializers.py
@@ -1,11 +1,9 @@
 from rest_framework import serializers
-from core.models import FileReference
+from core.models import FileReference, EventSystem
 
-class FileReferenceSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the FileReference model.
-    Handles serialization and deserialization of FileReference instances.
-    """
+
+
+class EventSystemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = FileReference
-        fields = '__all__'  # Include all fields or specify them explicitly
+        model = EventSystem
+        fields = '__all__'
