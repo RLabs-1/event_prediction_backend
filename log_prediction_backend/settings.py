8fedec975352e3095c0ea3a6a8fc8c8ec2c2e348
Ahmad4Ahmad <a_s3003@hotmail.com>
2025-01-07 16:56:03 +0200

    endpoint for uploading a file

--- log_prediction_backend/settings.py
+++ log_prediction_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,3 +151,6 @@ AUTHENTICATION_BACKENDS = [
     'user_management.backends.EmailBackend',     # Custom
 ]
 
+# Directory to store uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- user_management/views/views.py
+++ user_management/views/views.py
@@ -1,34 +1,16 @@
 # views.py
-from django.contrib.auth import get_user_model
-from django.views.decorators.csrf import csrf_exempt
-import json
-from django.http import JsonResponse
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import status, generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from user_management.services.services import RegistrationService
+from user_management.services.services import RegistrationService, UserService, JWTService
 from user_management.serializers.serializers import RegistrationSerializer, UserUpdateSerializer
-from rest_framework import generics,  permissions
-
 from user_management.models.models import User
-from user_management.services.services import UserService
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
-
 from core.models import User
-from django.shortcuts import render
-from user_management.services.services import UserService
-from django.contrib.auth import authenticate
-from user_management.services.services import JWTService
 from drf_spectacular.utils import extend_schema
-from django.shortcuts import render
-
-from rest_framework import status
-from django.contrib.auth import authenticate
+import json
 
 User = get_user_model()
-
-
 
 class RegistrationView(APIView):
     def post(self, request, *args, **kwargs):
@@ -40,6 +22,7 @@ class RegistrationView(APIView):
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 class UserUpdateView(generics.RetrieveUpdateAPIView):
     """View for updating User details"""
     permission_classes = (permissions.IsAuthenticated,)
@@ -63,6 +46,7 @@ class ActivateUserView(APIView):
         if service_response['success']:
             return Response(service_response, status=status.HTTP_200_OK)
         return Response(service_response, status=status.HTTP_400_BAD_REQUEST)
+    
 class UserLoginView(APIView):
     """
     Handles user login and returns a JWT token.
@@ -86,7 +70,6 @@ class UserLoginView(APIView):
         else:
             return Response({"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
 
-
 class ForgotPasswordView(APIView):
     def post(self, request):
         """
@@ -105,7 +88,6 @@ class ForgotPasswordView(APIView):
         if service_response['success']:
             return Response(service_response, status=status.HTTP_200_OK)
         return Response(service_response, status=status.HTTP_400_BAD_REQUEST)
-
 
 @extend_schema(
     summary="Reset Forgotten Password",
@@ -127,6 +109,7 @@ class ForgotPasswordView(APIView):
         410: {'description': 'Verification code expired'},
     }
 )
+
 class ResetForgotPasswordView(APIView):
     def post(self, request):
         try:
@@ -169,6 +152,3 @@ class ResetForgotPasswordView(APIView):
             return Response({"error": "Invalid JSON"}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-
