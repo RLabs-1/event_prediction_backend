7a348611bc8f830c6f6e0b18be94c8af757c0816
newpc <osmanlama.2000@gmail.com>
2025-01-07 12:02:53 +0200

    Fix serializer updates after merge

--- file_manager/serializers/serializers.py
+++ file_manager/serializers/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from core.models import FileReference, EventSystem
 
 
+class FileReferenceSerializer(serializers.ModelSerializer)
+    class Meta:
+        model = FileReference
+        fields = '__all__'
+
 
 class EventSystemSerializer(serializers.ModelSerializer):
     class Meta:
