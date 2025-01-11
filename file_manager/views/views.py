7e4ccfbbcd080c7048a5753c1fbab8e5d18d3e00
Abd El Naser <abdelnaser@Abds-Mac-mini.local>
2025-01-11 16:40:49 +0200

    BET-52 Create endpoint for deleting a file

--- file_manager/views/views.py
+++ file_manager/views/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from user_management.services.services import EventSystemFileService
+
+class EventSystemFileView(APIView):
+    def delete(self, request, eventSystemId, fileId):
+        EventSystemFileService.delete_file(eventSystemId, fileId)
+        return Response({"message": "File deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
