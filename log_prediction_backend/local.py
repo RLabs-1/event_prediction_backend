6771cd61b8fae21e2e40de74fd58f619f38a6e63
Majd Zbedat <zbedat.majd2000@gmail.com>
2025-01-17 01:30:07 +0200

    Create Configuration Template file

---
+++ log_prediction_backend/local.py
@@ -0,0 +1,18 @@
+# Database configurations
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event_prediction_db',
+        'USER': 'postgres',
+        'PASSWORD': '147258magd',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Debug settings
+DEBUG = True
+
+# Email settings
+EMAIL_HOST_USER = 'eventprediction.backend@gmail.com'
+EMAIL_HOST_PASSWORD = 'ledz sibu oocn lcwo'
