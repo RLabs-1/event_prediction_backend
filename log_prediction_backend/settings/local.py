aa0f3fd8d328d89a9e62ce946d39d1a9f7272bbc
adankh4 <adankh4@gmail.com>
2025-03-13 13:48:56 +0200

    Update all instances of the enums in tables to hold integer values.

--- log_prediction_backend/settings/local.py
+++ log_prediction_backend/settings/local.py
@@ -3,9 +3,13 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-   }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event_prediction_db',
+        'USER': 'postgres',
+        'PASSWORD': 'adankh313',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
