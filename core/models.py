254d70971435f51f71cf9de96833343e4c3941ac
Raneem34 <53892084+Raneem34@users.noreply.github.com>
2025-03-17 21:11:47 +0200

    Creating Credentials Model, with encrypted secret_key

--- core/models.py
+++ core/models.py
@@ -8,6 +8,7 @@ from user_management.exceptions.custom_exceptions import (
     UserStateError,
     UserNotVerifiedError
 )
+from core.model.credentials_model import Credentials
 
 class UserManager(BaseUserManager):
     """ Manager for the Users in the system"""
@@ -317,3 +318,5 @@ class UserSystemPermissions(models.Model):
 
     def __str__(self):
         return f"{self.user.email} - {self.event_system.name} - {self.get_permission_level_display()}"
+
+
