c4d959bd77a59c9aaaab2d11dbcffb1b3eeb7a21
lianmassarwa <101951587+lianmassarwa@users.noreply.github.com>
2025-03-18 23:59:25 +0200

    Update credentials_serializers.py

--- user_management/serializers/credentials_serializers.py
+++ user_management/serializers/credentials_serializers.py
@@ -4,3 +4,17 @@ from core.model.credentials_model import Credentials
 
 
 
+
+class CredentialUpdateSerializer(serializers.ModelSerializer):
+    secret_key = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = Credentials
+        fields = ['id' , 'access_key', 'secret_key', 'storage']
+
+    def update(self, instance, validated_data):
+        if 'secret_key' in validated_data:
+            instance.set_secret_key(validated_data.pop('secret_key'))
+
+        return super().update(instance, validated_data)
+
