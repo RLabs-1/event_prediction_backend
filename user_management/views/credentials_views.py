83159fcf49252c0225c86985bedbef1401aa18ad
lianmassarwa <101951587+lianmassarwa@users.noreply.github.com>
2025-03-19 00:00:52 +0200

    Update credentials_views.py

--- user_management/views/credentials_views.py
+++ user_management/views/credentials_views.py
@@ -10,3 +10,23 @@ from rest_framework.permissions import IsAuthenticated
 from core.model.credentials_model import Credentials
 from rest_framework.generics import DestroyAPIView
 
+
+
+class UpdateCredentialView(generics.UpdateAPIView):
+    queryset = Credentials.objects.all()
+    serializer_class = CredentialUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, credentialId):
+        try:
+            credential = Credentials.objects.get(id=credentialId)
+        except Credentials.DoesNotExist:
+            return Response({"error": "Credential not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CredentialUpdateSerializer(credential, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Credentials updated successfully"}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
