990ef8ae0d162ef7ef871e624aac33dcb1020640
adankh4 <adankh4@gmail.com>
2025-03-28 12:05:41 +0300

    Saving work before pulling main

--- log_prediction_backend/settings/local.py
+++ log_prediction_backend/settings/local.py
@@ -4,7 +4,7 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_prediction_db',
+        'NAME': 'event_prediction_db2',
         'USER': 'postgres',
         'PASSWORD': 'adankh313',
         'HOST': 'localhost',
